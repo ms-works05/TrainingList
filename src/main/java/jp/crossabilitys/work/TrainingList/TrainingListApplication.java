7c5b7475cade0c26b87dd39f25bc38f02672e722
UserM <m.shimizu.cross@gmail.com>
2023-09-05 15:13:00 +0900

    新規作成

---
+++ src/main/java/jp/crossabilitys/work/TrainingList/TrainingListApplication.java
@@ -0,0 +1,13 @@
+package jp.crossabilitys.work.TrainingList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/crossabilitys/work/TrainingList/TrainingListApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.crossabilitys.work.TrainingList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
