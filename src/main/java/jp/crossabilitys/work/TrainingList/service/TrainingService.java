196299a797410c760e47f0c9c6514ed1b2872052
UserM <m.shimizu.cross@gmail.com>
2023-10-31 10:42:51 +0900

    ・訓練情報画面修正 （登録ボタン押下時、訓練期間変更なしの場合は全削除せず更新するように変更）

--- src/main/java/jp/crossabilitys/work/TrainingList/service/TrainingService.java
+++ src/main/java/jp/crossabilitys/work/TrainingList/service/TrainingService.java
@@ -36,21 +36,21 @@ public class TrainingService {
     private final TrainingListRepository listRepository;
     /**
      * 訓練情報 全検索
+     * 訓練マスタより全件取得
      * @return 検索結果
      */
     public List<TrainingInfo> searchAll() {
         return trainingRepository.searchAll(false);
-
     }
     /**
      * 訓練情報 全検索
-     * 講師未割当件数を取得
+     * 訓練マスタより全件取得
+     * 併せて訓練スケジュールテーブルの講師未割当件数取得
      * @return 検索結果
      */
     public List<TrainingList> searchAllwithAssign(){
         return listRepository.searchAllwithAssign(false);
     }
-
     /**
      * 訓練情報 ID検索
      * @param id ID
@@ -62,10 +62,14 @@ public class TrainingService {
     }
     /**
      * 訓練情報 登録
+     * [新規]訓練マスタ、訓練スケジュールテーブル新規登録
+     * [更新]訓練マスタ更新、訓練期間変更ありの場合、訓練スケジュールテーブル新規登録
+     *                    訓練期間変更なしの場合、訓練スケジュールテーブル更新
      * @param request
      */
     public void registTraining(TrainingRequest request) {
         TrainingInfo eList = new TrainingInfo();
+        boolean isAddSchedule = true;
 
         // 値保持
         LocalDate startDate = request.getStart_date();      // 訓練期間
@@ -75,13 +79,24 @@ public class TrainingService {
         // 祝日
         BusinessCalendar holidays = BusinessCalendar.newBuilder().holiday(BusinessCalendar.JAPAN.PUBLIC_HOLIDAYS).build();
 
+        /* ----- 登録前処理 ----- */
         // 更新の場合
         if (request.getId()!=0){
-            eList.setId(request.getId());
-            //訓練スケジュール削除
-            eList.getTrainingSchedule().clear();
-            trainingRepository.save(eList);
+//            eList.setId(request.getId());
+            // 更新前データ取得
+            eList = findById(request.getId());
+            if (eList.getStart_date().equals(startDate) && eList.getEnd_date().equals(endDate)) {
+                // 訓練期間変更なしの場合
+                isAddSchedule = false;
+            } else{
+                // 訓練期間変更ありの場合
+                isAddSchedule = true;
+                //訓練スケジュール削除(登録済スケジュールデータを全削除)
+                eList.getTrainingSchedule().clear();
+                trainingRepository.save(eList);
+            }
         }
+        /* ----- 訓練情報 登録 ----- */
         // Entityクラスに値セット
         eList.setTrainingname(request.getTrainingname());
         eList.setRecruit_start_date(request.getRecruit_start_date());
@@ -101,23 +116,33 @@ public class TrainingService {
         }else {
             eList.setConsignor_id(request.getConsignor_id());
         }
+        /* ----- 訓練スケジュール 登録 ----- */
+        if (isAddSchedule == true){
+            // 訓練スケジュールデータ新規作成
+            eList.getTrainingSchedule().clear();
+            for (LocalDate date = startDate; date.isBefore(endDate.plusDays(1)); date = date.plusDays(1)) {
+                TrainingSchedule oneDay = new TrainingSchedule();
+                oneDay.setTrainingInfo(eList);
+                oneDay.setTraining_date(date);
+//                oneDay.setMemo("");
+//                oneDay.setTeacher_id(0L);
 
-        // 訓練スケジュールデータ作成
-        eList.getTrainingSchedule().clear();
-        for (LocalDate date = startDate; date.isBefore(endDate.plusDays(1)); date = date.plusDays(1)) {
-            TrainingSchedule oneDay = new TrainingSchedule();
-            oneDay.setTrainingInfo(eList);
-            oneDay.setTraining_date(date);
-            oneDay.setMemo("");
-//            oneDay.setTeacher_id(0L);
-
-            if ((date.getDayOfWeek().getValue() > 5) || (holidays.isHoliday(oneDay.getTraining_date())) ) {
-                // 土日祝日の場合、訓練時間数をゼロに設定
-                oneDay.setTraining_hours(0);
-            }else{
-                oneDay.setTraining_hours(trainingHours);
+                if ((date.getDayOfWeek().getValue() > 5) || (holidays.isHoliday(oneDay.getTraining_date())) ) {
+                    // 土日祝日の場合、訓練時間数をゼロに設定
+                    oneDay.setTraining_hours(0);
+                }else{
+                    oneDay.setTraining_hours(trainingHours);
+                }
+                eList.getTrainingSchedule().add(oneDay);
             }
-            eList.getTrainingSchedule().add(oneDay);
+        } else {
+            // 訓練スケジュールデータ更新
+            for (TrainingSchedule oneDay : eList.getTrainingSchedule()){
+                if (oneDay.getTraining_hours()>0) {
+                    // 訓練時間更新
+                    oneDay.setTraining_hours(trainingHours);
+                }
+            }
         }
         TrainingInfo out = trainingRepository.save(eList);
     }
